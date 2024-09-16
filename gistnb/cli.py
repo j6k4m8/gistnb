5ba38133dd765f68425a801585b8a2d7f72ab0e5
Jordan Matelsky <j6k4m8@gmail.com>
2024-09-16 12:49:19 -0400

    feat: Add install / run commands

--- gistnb/cli.py
+++ gistnb/cli.py
@@ -1,4 +1,6 @@
+import datetime
 import pathlib
+import subprocess
 import click
 import nbformat as nbf
 import tempfile
@@ -68,9 +70,9 @@ def cli(ctx, notebook, venv_path):
 
 
 @cli.command()
-@click.argument("packages", nargs=-1)
+@click.argument("package")
 @click.pass_context
-def add(ctx, packages):
+def add(ctx, package):
     """Add packages to the notebook metadata."""
     notebook_path = ctx.obj["notebook"]
 
@@ -86,11 +88,8 @@ def add(ctx, packages):
 
     # Add the packages to the metadata
     dep_container = DepContainer.from_list(
-        nb.metadata[_METADATA_DIR][_METADATA_DEPLIST_PATH]
+        nb.metadata[_METADATA_DIR][_METADATA_DEPLIST_PATH] + [package]
     )
-    for package in packages:
-        pkg, ver, operator = _split_package_and_version(package)
-        dep_container.pin(pkg, (operator, ver))
     nb.metadata[_METADATA_DIR][_METADATA_DEPLIST_PATH] = dep_container.as_list()
 
     # Save the notebook
@@ -120,3 +119,102 @@ def guess(ctx):
 
     # Save the imports as dependencies
     print("Guessed dependencies:", imports)
+
+
+@cli.command()
+@click.pass_context
+def list(ctx):
+    """List packages in the notebook metadata."""
+    notebook_path = ctx.obj["notebook"]
+
+    # Load the notebook
+    with open(notebook_path, "r") as f:
+        nb = nbf.read(f, as_version=4)
+
+    # List the dependencies
+    dependencies = nb.metadata.get(_METADATA_DIR, {}).get(_METADATA_DEPLIST_PATH, [])
+    now = datetime.datetime.now().isoformat()
+    click.echo(f"# Dependencies as of {now}")
+    for dep in dependencies:
+        click.echo(f"{dep}")
+
+
+@cli.command()
+@click.pass_context
+@click.argument("package")
+def remove(ctx, package):
+    """Remove a package from the notebook metadata."""
+    notebook_path = ctx.obj["notebook"]
+
+    # Load the notebook
+    with open(notebook_path) as f:
+        nb = nbf.read(f, as_version=4)
+
+    # Remove the package from the metadata
+    dependencies = nb.metadata.get(_METADATA_DIR, {}).get(_METADATA_DEPLIST_PATH, [])
+    # TODO: Handle case where version is specified
+    dependencies = [dep for dep in dependencies if dep != package]
+    nb.metadata[_METADATA_DIR][_METADATA_DEPLIST_PATH] = dependencies
+
+    # Save the notebook
+    with open(notebook_path, "w") as f:
+        nbf.write(nb, f)
+
+
+@cli.command()
+@click.pass_context
+def clear(ctx):
+    """Clear all packages from the notebook metadata."""
+    notebook_path = ctx.obj["notebook"]
+
+    # Load the notebook
+    with open(notebook_path, "r") as f:
+        nb = nbf.read(f, as_version=4)
+
+    # Clear the dependencies
+    nb.metadata[_METADATA_DIR] = {}
+
+    # Save the notebook
+    with open(notebook_path, "w") as f:
+        nbf.write(nb, f)
+
+
+@cli.command()
+@click.pass_context
+def install(ctx):
+    """Install packages listed in the notebook metadata."""
+    notebook_path = ctx.obj["notebook"]
+    venv_path = ctx.obj["venv_path"]
+    venv_pip = pathlib.Path(venv_path) / "bin" / "pip"
+
+    # Load the notebook
+    with open(notebook_path, "r") as f:
+        nb = nbf.read(f, as_version=4)
+
+    # Get the dependencies
+    dependencies = nb.metadata.get(_METADATA_DIR, {}).get(_METADATA_DEPLIST_PATH, [])
+
+    # Install the dependencies using pip
+    for dep in dependencies:
+        click.echo(f"Installing {dep}...")
+        subprocess.run([str(venv_pip), "install", dep], check=True)
+
+
+@cli.command()
+@click.pass_context
+def run(ctx):
+    """Run the notebook."""
+    notebook_path = ctx.obj["notebook"]
+    venv_path = ctx.obj["venv_path"]
+    jupyter_path = pathlib.Path(venv_path) / "bin" / "jupyter"
+
+    # Run the notebook using jupyter
+    click.echo(f"Running notebook {notebook_path}...")
+    subprocess.run(
+        [
+            str(jupyter_path),
+            "notebook",
+            str(notebook_path),
+        ],
+        check=True,
+    )
